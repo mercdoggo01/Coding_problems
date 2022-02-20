749e2aeac5c1756cad69c36b8c4fdd10e58fd788
Shivaditya08 <98040347+Shivaditya08@users.noreply.github.com>
2022-02-20 21:21:21 +0530

    Add files via upload

---
+++ CPP/Basics/sum_of_two_number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"\n\n Print the sum of two number :\n";
+    cout<<"----------------------------------\n";
+    int a;
+    int b;
+    int sum;
+    a=29;
+    b=30;
+    sum=a+b;
+    cout<<" the sum of "<< a << " and "<< b <<" is:"<<sum<<"\n\n;";
+}
