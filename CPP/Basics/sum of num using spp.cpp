3ba021594dac32a0207789f4ed0482f5da4468cf
OMMARYAN <95135298+OMMARYAN@users.noreply.github.com>
2022-02-21 09:15:03 +0530

    Add files via upload
    this contain normal syntax of cpp

---
+++ CPP/Basics/sum of num using spp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1 , num2;
+cout<<"enter the value of num1\n";
+cin>>num1;
+cout<<"enter the value of num2\n";
+cin>>num2;
+cout<<"The sum is "<< num1 + num2 ;
+return 0;
+}
