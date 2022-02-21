58facb6a79afb35874ba85909713767ddc81c0ea
OMMARYAN <95135298+OMMARYAN@users.noreply.github.com>
2022-02-21 09:54:20 +0530

    Add files via upload

---
+++ CPP/Basics/arithmatic operrators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=4 ,b=5;
+cout<<"operator in c++"<< endl;
+cout<<"the value of a + b is "<< a+b <<endl ;
+cout<<"the value of a - b is "<< a-b <<endl ;
+cout<<"the value of a * b is "<< a*b <<endl ;
+cout<<"the value of a / b is "<< a/b <<endl ;
+cout<<"the value of a ++ is " << a++ <<endl ;
+cout<<"the value of a-- is " << a-- <<endl ;
+cout<<"the value of ++a is " << ++a <<endl ;
+cout<<"the value of --a is " << --a <<endl ;
+return 0;
+}
