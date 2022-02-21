b94a5efa809da420c34e2d59c674073db6414cff
OMMARYAN <95135298+OMMARYAN@users.noreply.github.com>
2022-02-21 10:43:39 +0530

    Add files via upload

---
+++ CPP/Basics/comparison op.cpp
@@ -0,0 +1,15 @@
+//assignement opertors
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=3 , b=9;
+    cout<<"the value of a==b is "<<(a==b)<<endl;
+    cout<<"the value of a!=b is "<<(a!=b)<<endl;
+     cout<<"the value of a>b is "<<(a>b)<<endl;
+      cout<<"the value of a<b is "<<(a<b)<<endl;
+       cout<<"the value of a<=b is "<<(a<=b)<<endl;
+        cout<<"the value of a>=b is "<<(a>=b)<<endl;
+
+return 0;
+}
